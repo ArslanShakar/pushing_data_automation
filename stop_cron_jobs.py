6c41303745259c1e925cd2fb38ed9e296494ba4a
ArslanShakar <mrarslan101@gmail.com>
2020-07-09 21:58:48 +0500

    Cron jobs updated.

--- stop_cron_jobs.py
+++ stop_cron_jobs.py
@@ -5,7 +5,7 @@ my_cron = CronTab(user=True)
 for job in my_cron:
     if job.comment == "push_data_automation":
         my_cron.remove(job)
+        print('CRON JOB with comment "push_data_automation" has been removed.')
 
 # my_cron.remove_all()
 my_cron.write()
-print("All CRON JOBS has been removed successfully")
